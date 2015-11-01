9e5c71012faaf87dfe48f666ca9f3fe4367b8a58
investdice <yeskt2@gmail.com>
2015-11-01 03:02:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ var config = {
   // - Configure the house edge (default is 1%)
   //   Must be between 0.0 (0%) and 1.0 (100%)
   house_edge: 0.01,
-  chat_buffer_size: 75,
+  chat_buffer_size: 50,
   // - The amount of bets to show on screen in each tab
-  bet_buffer_size: 50
+  bet_buffer_size: 25
 };
 
 ////////////////////////////////////////////////////////////
